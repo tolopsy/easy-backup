aae70d9fef03db068c41a96cff97ea46067a6891
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-06-13 14:56:07 +0100

    feat: add path utility package

---
+++ path_utils/utils.go
@@ -0,0 +1,22 @@
+package pathutils
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+type Path struct {
+	Path string
+	Hash string
+}
+
+func (p Path) String() string {
+	return fmt.Sprintf("%s [%s]", p.Path, p.Hash)
+}
+
+func GetAbsPath(name string) string {
+	absPath, _ := filepath.Abs(name)
+	return absPath
+}
+
+var PathFileName = "Paths"
